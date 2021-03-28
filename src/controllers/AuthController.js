1fe8fbb93896ccb89b4679450f98d7c5e975d2ff
theluposki <theluposki@gmail.com>
2021-03-27 21:17:04 -0300

    registro de usuário

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,7 +1,15 @@
+import { register } from "../repository/AuthRepository"
+
 export const Auth = (req, res) => {
     res.status(200).json({ auth: true })
 }
 
-export const Register = (req, res) => {
-    res.status(201).json({ register: true })
+export const Register = async (req, res) => {
+    const body = req.body
+    try {
+        const result = await register(body)
+        res.status(201).json({ register: true, result })
+    } catch (error) {
+        res.status(400).json({ error: `Erro ao registrar o usuário > [${error}]` })
+    }
 }
